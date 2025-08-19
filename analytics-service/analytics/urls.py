c2a5c83d1800351ec3f0d8e9f2255587764a82e8
Shlok Trivedi <shloktrivedi2020@gmail.com>
2025-08-19 21:38:26 +0530

    remove django

--- analytics-service/analytics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HealthCheckView, AnalyticsDashboardView, SimpleChartView
-
-urlpatterns = [
-    path('health/', HealthCheckView.as_view(), name='health-check'),
-    path('dashboard/', AnalyticsDashboardView.as_view(), name='analytics-dashboard'),
-    path('charts/<str:chart_type>/', SimpleChartView.as_view(), name='simple-chart'),
-]
